a163f280dc5b60bf67b05a98cad0d036f50f598f
aston <astonbitecode@gmail.com>
2024-11-01 12:48:34 +0200

    Use j4rs 0.20.0

--- j4rstestnative/build.sh
+++ j4rstestnative/build.sh
@@ -2,10 +2,14 @@
 rustdir="$(dirname "$0")"
 cd "$rustdir"
 
+cargo build --target=armv7-linux-androideabi
 cargo build --target=aarch64-linux-android
+cargo build --target=i686-unknown-linux-gnu
 cargo build
 
 mkdir -p ../app/src/main/jniLibs/{arm64-v8a,armeabi,armeabi-v7a,x86_64}
 
+cp ./target/armv7-linux-androideabi/debug/libj4rstest.so ../app/src/main/jniLibs/armeabi-v7a
 cp ./target/aarch64-linux-android/debug/libj4rstest.so ../app/src/main/jniLibs/arm64-v8a
+cp ./target/i686-unknown-linux-gnu/debug/libj4rstest.so ../app/src/main/jniLibs/x86
 cp ./target/debug/libj4rstest.so ../app/src/main/jniLibs/x86_64
